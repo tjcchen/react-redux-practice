afcc8781a672a7e23d7a8900222f65213ea9e8d7
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-17 11:49:05 +0800

    feat: add scaffolding for the project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const path                   = require('path');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: path.join(__dirname, './src/index/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true
+      }
+    }),
+    new CleanWebpackPlugin()
+  ],
+  stats: 'normal'
+};
